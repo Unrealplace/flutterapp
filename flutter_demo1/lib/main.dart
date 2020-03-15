4d0e82e1a4c4f28be40b6f5217efc9e5a75bc726
liyang <liyang@adnonstop.com>
2020-03-15 21:57:46 +0800

    listview 添加跳转

--- flutter_demo1/lib/main.dart
+++ flutter_demo1/lib/main.dart
@@ -11,7 +11,70 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.red,
       ),
-      home: new Index(),
+      home: MyMainPage(_getSkipList()),
     );
+  }
+
+  List _getSkipList() {
+    List<String> titles = ["第一页面", "第二个页面"];
+    List<String> routeNames = ["first_page", "second_page"];
+    List<SkipModel> listModel = [];
+    for (int i = 0; i < titles.length; i++) {
+      SkipModel model = SkipModel(routeNames[i], titles[i]);
+      listModel.add(model);
+    }
+
+    return listModel;
+  }
+}
+
+class SkipModel {
+  String routName;
+  String title;
+
+  SkipModel(this.routName, this.title);
+}
+
+class MyMainPage extends StatelessWidget {
+  final List<SkipModel> dataSource;
+
+  MyMainPage(this.dataSource);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('主页'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.builder(
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: () {
+                _skipWithIndex(index);
+              },
+              child: Container(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("${dataSource[index].title}"),
+                    Divider(),
+                  ],
+                ),
+
+              ),
+            );
+          },
+          itemCount: dataSource.length,
+        ),
+      ),
+    );
+  }
+
+  void _skipWithIndex(int index) {
+    swit
   }
 }
